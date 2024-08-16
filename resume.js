bff9d1aac2cf59ae04423257185c7399dbeb5acc
isabella-zaens <144853593+isabella-zaens@users.noreply.github.com>
2024-08-16 22:37:56 +0000

    remove animation

--- resume.js
+++ resume.js
@@ -1,37 +1,33 @@
-function toggleMode() {
-    const body = document.body;
-    const header = document.querySelector('header');
-    const sections = document.querySelectorAll('section');
-    const pElements = document.querySelectorAll('p');
-    const aElements = document.querySelectorAll('header a');
-    const toggleIcon = document.getElementById('toggle-icon');
-    
-    const isDarkMode = body.classList.toggle('dark-mode');
+window.toggleMode = function() {
+  const body = document.body;
+  const header = document.querySelector('header');
+  const sections = document.querySelectorAll('section');
+  const pElements = document.querySelectorAll('p');
+  const aElements = document.querySelectorAll('header a');
+  const toggleIcon = document.getElementById('toggle-icon');
+  
+  const isDarkMode = body.classList.toggle('dark-mode');
 
-    body.style.backgroundColor = isDarkMode ? '#000' : '#fff';
-    body.style.color = isDarkMode ? '#fff' : '#000';
-    header.style.backgroundColor = isDarkMode ? '#000' : '#fff';
+  body.style.backgroundColor = isDarkMode ? '#000' : '#fff';
+  body.style.color = isDarkMode ? '#fff' : '#000';
+  header.style.backgroundColor = isDarkMode ? '#000' : '#fff';
 
-    sections.forEach(section => {
-        section.classList.toggle('dark-mode', isDarkMode);
-    });
+  sections.forEach(section => {
+      section.classList.toggle('dark-mode', isDarkMode);
+  });
 
-    pElements.forEach(p => {
-        p.style.color = isDarkMode ? '#fff' : '#000';
-    });
+  pElements.forEach(p => {
+      p.style.color = isDarkMode ? '#fff' : '#000';
+  });
 
-    aElements.forEach(a => {
-        a.style.color = isDarkMode ? '#fff' : '#000';
-    });
+  aElements.forEach(a => {
+      a.style.color = isDarkMode ? '#fff' : '#000';
+  });
 
-    // Toggle between sun and moon icons
-    toggleIcon.classList.toggle('fa-sun', isDarkMode);
-    toggleIcon.classList.toggle('fa-moon', !isDarkMode);
-}
-
-// function goHome() {
-//     window.location.href = "index.html";
-// }
+  // Toggle between sun and moon icons
+  toggleIcon.classList.toggle('fa-sun', isDarkMode);
+  toggleIcon.classList.toggle('fa-moon', !isDarkMode);
+};
 
 window.goHome = function() {
   window.location.href = "index.html";
